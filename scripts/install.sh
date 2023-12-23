0efa0522a85577e560493aa3c3ded15510684a54
チセ <123655015+chise0713@users.noreply.github.com>
2023-12-23 12:38:15 +0800

    Install script: Try to support macos
    If something went wrong, submit a issue refer this commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,81 +1,100 @@
 #!/bin/bash
 set -e
 cd /tmp
-if [[ "$(uname)" != 'Linux' ]]; then
+OS=$(uname)
+
+if [[ "$OS" != 'Linux' ]] && [[ "$OS" != 'Darwin' ]]; then
     echo "error: This operating system is not supported."
     exit 1
 fi
+
+if [[ "$OS" == 'Darwin' ]]; then
+    OS='macos'
+elif [[ "$OS" == 'Linux' ]]; then
+    OS='linux'
+fi
+
 case "$(uname -m)" in
     'i386' | 'i686')
         MACHINE='32'
-    ;;
+        ;;
     'amd64' | 'x86_64')
         MACHINE='64'
-    ;;
+        ;;
     'armv7' | 'armv7l')
         MACHINE='arm32-v7a'
         grep Features /proc/cpuinfo | grep -qw 'vfp' || MACHINE='arm32-v5'
-    ;;
+        ;;
     'armv8' | 'aarch64')
         MACHINE='arm64-v8a'
-    ;;
-  *)
-    echo "error: The architecture is not supported."
-    exit 1
-    ;;
-esac
-
-package_manager() {
-    if [[ "$(type -P apt)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='apt -y --no-install-recommends install'
-        PACKAGE_MANAGEMENT_REMOVE='apt purge'
-        package_provide_tput='ncurses-bin'
-    elif [[ "$(type -P dnf)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='dnf -y install'
-        PACKAGE_MANAGEMENT_REMOVE='dnf remove'
-        package_provide_tput='ncurses'
-    elif [[ "$(type -P yum)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='yum -y install'
-        PACKAGE_MANAGEMENT_REMOVE='yum remove'
-        package_provide_tput='ncurses'
-    elif [[ "$(type -P zypper)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='zypper install -y --no-recommends'
-        PACKAGE_MANAGEMENT_REMOVE='zypper remove'
-        package_provide_tput='ncurses-utils'
-    elif [[ "$(type -P pacman)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='pacman -Syu --noconfirm'
-        PACKAGE_MANAGEMENT_REMOVE='pacman -Rsn'
-        package_provide_tput='ncurses'
-     elif [[ "$(type -P emerge)" ]]; then
-        PACKAGE_MANAGEMENT_INSTALL='emerge -qv'
-        PACKAGE_MANAGEMENT_REMOVE='emerge -Cv'
-        package_provide_tput='ncurses'
-    else
-        echo -e "${ERROR}ERROR:${END} The script does not support the package manager in this operating system."
+        ;;
+    *)
+        echo "error: The architecture is not supported."
         exit 1
-    fi
-}
+        ;;
+esac
+if [[ "$(type -P apt)" ]]; then
+    PACKAGE_MANAGEMENT_INSTALL='apt -y --no-install-recommends install'
+    PACKAGE_MANAGEMENT_REMOVE='apt purge'
+    package_provide_tput='ncurses-bin'
+elif [[ "$(type -P dnf)" ]]; then
+    PACKAGE_MANAGEMENT_INSTALL='dnf -y install'
+    PACKAGE_MANAGEMENT_REMOVE='dnf remove'
+    package_provide_tput='ncurses'
+elif [[ "$(type -P yum)" ]]; then
+    PACKAGE_MANAGEMENT_INSTALL='yum -y install'
+    PACKAGE_MANAGEMENT_REMOVE='yum remove'
+    package_provide_tput='ncurses'
+elif [[ "$(type -P zypper)" ]]; then
+    PACKAGE_MANAGEMENT_INSTALL='zypper install -y --no-recommends'
+    PACKAGE_MANAGEMENT_REMOVE='zypper remove'
+    package_provide_tput='ncurses-utils'
+elif [[ "$(type -P pacman)" ]]; then
+    PACKAGE_MANAGEMENT_INSTALL='pacman -Syu --noconfirm'
+    PACKAGE_MANAGEMENT_REMOVE='pacman -Rsn'
+    package_provide_tput='ncurses'
+elif [[ "$(type -P emerge)" ]]; then
+    PACKAGE_MANAGEMENT_INSTALL='emerge -qv'
+    PACKAGE_MANAGEMENT_REMOVE='emerge -Cv'
+    package_provide_tput='ncurses'
+elif [[ "$(type -P brew)" ]]; then
+    PACKAGE_MANAGEMENT_INSTALL='brew install'
+    PACKAGE_MANAGEMENT_REMOVE='brew uninstall'
+    package_provide_tput='ncurses'
+else
+    echo -e "error: The script does not support the package manager in this operating system."
+    exit 1
+fi
 if [[ $EUID -ne 0 ]]; then
-    echo -e "ERROR: You have to use root to run this script"
+    echo -e "error: You have to use root to run this script"
     exit 1
 fi
 install_software() {
     package_name="$1"
     file_to_detect="$2"
-    type -P "$file_to_detect" > /dev/null 2>&1 && return || echo -e "WARN: $package_name not installed, installing." && sleep 1
+    type -P "$file_to_detect" > /dev/null 2>&1 && return || echo -e "warn: $package_name not installed, installing." && sleep 1
     if ${PACKAGE_MANAGEMENT_INSTALL} "$package_name"; then
-        echo "INFO: $package_name is installed."
+        echo "error: $package_name is installed."
     else
-        echo -e "ERROR: Installation of $package_name failed, please check your network."
+        echo -e "error: Installation of $package_name failed, please check your network."
         exit 1
     fi
 }
-package_manager
+
 install_software 'unzip' 'unzip'
 ZIP_FILE="wgcf-cli.zip"
-curl -Lo $ZIP_FILE https://github.com/chise0713/wgcf-cli/releases/latest/download/wgcf-cli-linux-${MACHINE}.zip
-curl -Lo $ZIP_FILE.dgst https://github.com/chise0713/wgcf-cli/releases/latest/download/wgcf-cli-linux-${MACHINE}.zip.dgst
+curl -Lo $ZIP_FILE https://github.com/ArchiveNetwork/wgcf-cli/releases/latest/download/wgcf-cli-${OS}-${MACHINE}.zip
+curl -Lo $ZIP_FILE.dgst https://github.com/ArchiveNetwork/wgcf-cli/releases/latest/download/wgcf-cli-${OS}-${MACHINE}.zip.dgst
 CHECKSUM=$(awk -F '= ' '/256=/ {print $2}' "${ZIP_FILE}.dgst")
-LOCALSUM=$(sha256sum "$ZIP_FILE" | awk '{printf $1}')
+[[ $OS == 'macos' ]] && LOCALSUM=$((shasum -a 256 wgcf-cli.zip | awk '{printf $1}')|| (sha256sum wgcf-cli.zip | awk '{printf $1}'))
+[[ $OS == 'linux' ]] && LOCALSUM=$(sha256sum "$ZIP_FILE" | awk '{printf $1}')
+if [[ "$CHECKSUM" != "$LOCALSUM" ]]; then
+    echo -e "error: Checksums do not match, please check your network."
+    exit 1
+fi
 unzip -q $ZIP_FILE -d /tmp/wgcf-cli
 mv /tmp/wgcf-cli/wgcf-cli /usr/local/bin/wgcf-cli
+rm -rf /tmp/wgcf-cli/
+rm -f $ZIP_FILE
+rm -f $ZIP_FILE.dgst
+echo -e "info: wgcf-cli installed."
