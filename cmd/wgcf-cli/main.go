63b7958312fa9da9c7b86b56cea56283502f1f4a
AVAtarMod <Respawner289@gmail.com>
2024-10-08 08:56:21 +0300

    main: Use constant for config path
    (cherry picked from commit fe53a7b02a27310788f66649d6d675c1d5b67193)

--- cmd/wgcf-cli/main.go
+++ cmd/wgcf-cli/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const ConfigPathDefault string = "wgcf.json"
+
 var rootCmd = &cobra.Command{
 	Use:  os.Args[0],
 	Long: "A command-line tool for Cloudflare-WARP API, built using Cobra.",
@@ -18,7 +20,7 @@ var (
 )
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "wgcf.json", "set configuration file path")
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", ConfigPathDefault, "set configuration file path")
 }
 func main() {
 	rootCmd.Execute()
