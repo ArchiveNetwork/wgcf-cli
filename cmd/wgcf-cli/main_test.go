4091f3932d5bce8f9c2faaa69aaa4f26516f09b0
AVAtarMod <Respawner289@gmail.com>
2024-10-08 08:57:55 +0300

    tests: Fix generate command tests
    Refactor tests. Move generate command executions to distinct tests
    
    (cherry picked from commit ed73d8a1737080570536202b2ab35ebd9fd3ecca)

--- cmd/wgcf-cli/main_test.go
+++ cmd/wgcf-cli/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"testing"
@@ -11,6 +12,41 @@ import (
 	"github.com/ArchiveNetwork/wgcf-cli/utils"
 )
 
+func expectNoErr(err error, t *testing.T) {
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+}
+
+type checker func(err error)
+
+func createConfig(check checker) {
+	if _, err := os.Open(ConfigPathDefault); errors.Is(err, os.ErrNotExist) {
+		rootCmd.SetArgs([]string{"register"})
+		check(rootCmd.Execute())
+	}
+}
+func removeConfig(check checker) {
+	rootCmd.SetArgs([]string{"cancel", "--yes"})
+	check(rootCmd.Execute())
+}
+
+func beginGenerateTest(check checker) {
+	generateCmd.Flags().Set(asString(Xray), "false")
+	generateCmd.Flags().Set("wg", "false")
+	generateCmd.Flags().Set(asString(WgQuick), "false")
+	generateCmd.Flags().Set(asString(SingBox), "false")
+	createConfig(check)
+}
+func endGenerateTest(check checker, generator GeneratorType) {
+	removeConfig(check)
+	os.Remove(getDefaultFilePath(generator))
+}
+func runGenerateTest(check checker, generator GeneratorType, test func()) {
+	beginGenerateTest(check)
+	test()
+	endGenerateTest(check, generator)
+}
 func TestRootCmd(t *testing.T) {
 	var output bytes.Buffer
 	rootCmd.SetOutput(&output)
@@ -24,42 +60,59 @@ func TestRootCmd(t *testing.T) {
 		}
 		return response.Account.License
 	}
-	must := func(err error) {
-		if err != nil {
-			t.Errorf("Expected no error, but got: %v", err)
-		}
-	}
+
+	check := func(err error) { expectNoErr(err, t) }
 
 	rootCmd.SetArgs([]string{"register"})
-	must(rootCmd.Execute())
+	check(rootCmd.Execute())
 
 	rootCmd.SetArgs([]string{"simplify"})
-	must(rootCmd.Execute())
+	check(rootCmd.Execute())
 
 	rootCmd.SetArgs([]string{"name", "-n", "test"})
-	must(rootCmd.Execute())
+	check(rootCmd.Execute())
 
 	rootCmd.SetArgs([]string{"bind"})
-	must(rootCmd.Execute())
+	check(rootCmd.Execute())
 
 	rootCmd.SetArgs([]string{"unbind", "--yes"})
-	must(rootCmd.Execute())
+	check(rootCmd.Execute())
 
 	rootCmd.SetArgs([]string{"license", "-l", getLicense()})
-	must(rootCmd.Execute())
-
-	rootCmd.SetArgs([]string{"generate", "--wg"})
-	must(rootCmd.Execute())
-
-	rootCmd.SetArgs([]string{"generate", "--wg-quick"})
-	must(rootCmd.Execute())
-
-	rootCmd.SetArgs([]string{"generate", "--sing-box"})
-	must(rootCmd.Execute())
-
-	rootCmd.SetArgs([]string{"generate", "--xray"})
-	must(rootCmd.Execute())
+	check(rootCmd.Execute())
 
 	rootCmd.SetArgs([]string{"cancel", "--yes"})
-	must(rootCmd.Execute())
+	check(rootCmd.Execute())
+}
+
+func TestGenerateWg(t *testing.T) {
+
+	check := func(err error) { expectNoErr(err, t) }
+
+	runGenerateTest(check, WgQuick, func() {
+		rootCmd.SetArgs([]string{"generate", "--wg"})
+		check(rootCmd.Execute())
+
+		os.Remove(getDefaultFilePath(WgQuick))
+		rootCmd.SetArgs([]string{"generate", "--wg-quick"})
+		check(rootCmd.Execute())
+	})
+}
+
+func TestGenerateSingBox(t *testing.T) {
+	check := func(err error) { expectNoErr(err, t) }
+
+	runGenerateTest(check, WgQuick, func() {
+		rootCmd.SetArgs([]string{"generate", "--sing-box"})
+		check(rootCmd.Execute())
+	})
+}
+
+func TestGenerateXray(t *testing.T) {
+	check := func(err error) { expectNoErr(err, t) }
+
+	runGenerateTest(check, WgQuick, func() {
+		rootCmd.SetArgs([]string{"generate", "--xray"})
+		check(rootCmd.Execute())
+	})
 }
